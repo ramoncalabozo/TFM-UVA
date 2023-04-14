ba1d5f49c5c24c77a7171b143b4595b7e7bb335f
ramoncalabozo <ramoncalabozo@gmail.com>
2023-04-14 15:29:45 +0200

    Longitudes de onda y Aod

--- parametros.py
+++ parametros.py
@@ -3,17 +3,8 @@ import os
 
 def read_output(outfile):
     
-    # variables de entrada
-    radioComponent1= ""
-    sigmaComponent1 = ""
-    radioComponent2= ""
-    sigmaComponent2 = ""
-
-    #variables de salida
-    aod_mode_1 = ""
-    aod_mode_2 = ""
-    ssa_mode_1 = ""
-    ssa_mode_2 = ""
+    ssa_mode_1_L1 = ""
+    ssa_mode_2_L1 = ""
     g = ""
 
 
@@ -53,7 +44,60 @@ def read_output(outfile):
                 else :
                     sigmaComponent2 = separacion[1].strip()
 
-    dataset = str(radioComponent1) + " " + str(sigmaComponent1) + "    ---------------   " + str(radioComponent2) + " " + str(sigmaComponent2)
+        carac = "Wavelength (um), AOD_Particle_mode_1 (unitless or 1/um)"
+        if carac in line:
+            for j in range(4):
+                i+=1
+                line=content[i].strip()
+                separacion = line.split("   ")
+                if j == 0 :
+                    L1 = separacion[0].strip()
+                    aod_mode_1_L1  = separacion[1].strip()
+                elif j == 1 :
+                    L2 = separacion[0].strip()
+                    aod_mode_1_L2 = separacion[1].strip()
+                elif j == 2 :
+                    L3 = separacion[0].strip()
+                    aod_mode_1_L3 = separacion[1].strip()
+                else:
+                    L4 = separacion[0].strip()
+                    aod_mode_1_L4 = separacion[1].strip()
+
+        carac = "Wavelength (um), AOD_Particle_mode_2 (unitless or 1/um)"
+        if carac in line:
+            for j in range(4):
+                i+=1
+                line=content[i].strip()
+                separacion = line.split("   ")
+                if j == 0 :
+                    aod_mode_2_L1  = separacion[1].strip()
+                elif j == 1 :
+                    aod_mode_2_L2 = separacion[1].strip()
+                elif j == 2 :
+                    aod_mode_2_L3 = separacion[1].strip()
+                else:
+                    aod_mode_2_L4 = separacion[1].strip()
+      
+        
+
+    # Modo 1 -- L1
+    dataset =  str(radioComponent1) + " " + str(sigmaComponent1) + " " + str(L1) + " " + str(aod_mode_1_L1)  + "\n"
+    # Modo 1 -- L2
+    dataset += str(radioComponent1) + " " + str(sigmaComponent1) + " " + str(L2) + " " + str(aod_mode_1_L2)  + "\n"
+    # Modo 1 -- L3
+    dataset += str(radioComponent1) + " " + str(sigmaComponent1) + " " + str(L3) + " " + str(aod_mode_1_L3)  + "\n"
+    # Modo 1 -- L4
+    dataset += str(radioComponent1) + " " + str(sigmaComponent1) + " " + str(L4) + " " + str(aod_mode_1_L4)  + "\n"
+    
+    # Modo 2 -- L1
+    dataset +=  str(radioComponent2) + " " + str(sigmaComponent2) + " " + str(L1) + " " + str(aod_mode_2_L1)  + "\n"
+    # Modo 2 -- L2
+    dataset += str(radioComponent2) + " " + str(sigmaComponent2) + " " + str(L2) + " " + str(aod_mode_2_L2)  + "\n"
+    # Modo 2 -- L3
+    dataset += str(radioComponent2) + " " + str(sigmaComponent2) + " " + str(L3) + " " + str(aod_mode_2_L3)  + "\n"
+    # Modo 2 -- L4
+    dataset += str(radioComponent2) + " " + str(sigmaComponent2) + " " + str(L4) + " " + str(aod_mode_2_L4)  + "\n"    
+    
     return dataset 
 
 if __name__ == '__main__':
