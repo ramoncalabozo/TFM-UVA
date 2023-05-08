ad5a089032b3e295dbd382fb6d2b3305b090edef
ramoncalabozo <ramoncalabozo@gmail.com>
2023-05-08 09:32:16 +0200

    checkPoint redNeuronal

--- redneuronal.py
+++ redneuronal.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 from tensorflow import keras
 import numpy as np
 
-NUM = 50000
+NUM = 372360
 
 def crearModelo(entradaEntrenamiento, salidaEntrenamiento, entradaTest, salidaTest):
 
@@ -22,18 +22,27 @@ def crearModelo(entradaEntrenamiento, salidaEntrenamiento, entradaTest, salidaTe
     model.compile(optimizer='adam',
                 loss='mse',
                 metrics=['accuracy'])
-    
-     # Entrenar el modelo con los datos de entrenamiento
-    history = model.fit(entradaEntrenamiento, salidaEntrenamiento, epochs=50, batch_size=32, validation_split=0.2)
 
+    # Añadir checkpoint 
+    checkpoint =  tf.keras.callbacks.ModelCheckpoint('MejorVersionModelo.h5', monitor="val_loss", verbose=1, save_best_only=True, save_weights_only=False, mode="auto", period=1)
+   #ESCRIBO EN UN FICHERO LOS LOGROS QUE VA HACIENDO EL MODELO
+    csv_logger = tf.keras.callbacks.CSVLogger('log_model.txt', append=True, separator=',')
+
+    early_stopping = tf.keras.callbacks.EarlyStopping(monitor="val_loss", patience=10)
+    reduce_lr = tf.keras.callbacks.ReduceLROnPlateau(monitor="val_loss", patience=5)
+
+    # Entrenar el modelo con los datos de entrenamiento
+    history = model.fit(entradaEntrenamiento, salidaEntrenamiento, epochs=500, batch_size=32, validation_split=0.1,callbacks=[checkpoint,early_stopping, reduce_lr,csv_logger])
+    
     print("\n\n\nEVALUACIÓN: \n")
+    model=tf.keras.models.load_model( 'MejorVersionModelo.h5')
      # Evaluar el modelo con los datos de test
     test_loss, test_acc = model.evaluate(entradaTest, salidaTest)
     print("Test_accuracy: " +  str(test_acc) + "\n" + "Test_losser " + str(test_loss))
           
 
 def datosEntrada(archivoDataSet):
-    numEntrenamiento = NUM * 0.7    
+    numEntrenamiento = NUM * 0.8
     entradaEntrenamiento = []
     salidaEntrenamiento = []
     entradaTest = []
@@ -108,49 +117,49 @@ def normalizarDatos(entrada):
 
     j = 0
     for i in range(len(entrada)): 
-      if j <= 3:
-        entrada[i][0] = entrada[i][0] / radioComponent1Max
-        entrada[i][1] = entrada[i][1] / sigmaComponent1Max
-        entrada[i][2] = entrada[i][2] / esfericidadComponent1Max
-        entrada[i][3] = entrada[i][3] / concentracionComponent1Max
-        if j == 0:
-            entrada[i][4] = entrada[i][4] / L1Max
-            entrada[i][5] = entrada[i][5] / indRefReal_mode1_L1Max
-            entrada[i][6] = entrada[i][6] / indRefImag_mode1_L1Max
-        if j == 1:
-            entrada[i][4] = entrada[i][4] / L2Max
-            entrada[i][5] = entrada[i][5] / indRefReal_mode1_L2Max
-            entrada[i][6] = entrada[i][6] / indRefImag_mode1_L2Max
-        if j == 2:
-            entrada[i][4] = entrada[i][4] / L3Max
-            entrada[i][5] = entrada[i][5] / indRefReal_mode1_L3Max
-            entrada[i][6] = entrada[i][6] / indRefImag_mode1_L3Max
-        if j == 3:
-            entrada[i][4] = entrada[i][4] / L4Max
-            entrada[i][5] = entrada[i][5] / indRefReal_mode1_L4Max
-            entrada[i][6] = entrada[i][6] / indRefImag_mode1_L4Max
-    else:
-        entrada[i][0] = entrada[i][0] / radioComponent2Max
-        entrada[i][1] = entrada[i][1] / sigmaComponent2Max
-        entrada[i][2] = entrada[i][2] / esfericidadComponent2Max
-        entrada[i][3] = entrada[i][3] / concentracionComponent2Max
-        if j == 4:
-            entrada[i][4] = entrada[i][4] / L1Max
-            entrada[i][5] = entrada[i][5] / indRefReal_mode2_L1Max
-            entrada[i][6] = entrada[i][6] / indRefImag_mode2_L1Max
-        if j == 5:
-            entrada[i][4] = entrada[i][4] / L2Max
-            entrada[i][5] = entrada[i][5] / indRefReal_mode2_L2Max
-            entrada[i][6] = entrada[i][6] / indRefImag_mode2_L2Max
-        if j == 6:
-            entrada[i][4] = entrada[i][4] / L3Max
-            entrada[i][5] = entrada[i][5] / indRefReal_mode2_L3Max
-            entrada[i][6] = entrada[i][6] / indRefImag_mode2_L3Max
-        if j == 7:
-            entrada[i][4] = entrada[i][4] / L4Max
-            entrada[i][5] = entrada[i][5] / indRefReal_mode2_L4Max
-            entrada[i][6] = entrada[i][6] / indRefImag_mode2_L4Max
-            j = -1                    
+        if j <= 3:
+            entrada[i][0] = entrada[i][0] / radioComponent1Max
+            entrada[i][1] = entrada[i][1] / sigmaComponent1Max
+            entrada[i][2] = entrada[i][2] / esfericidadComponent1Max
+            entrada[i][3] = entrada[i][3] / concentracionComponent1Max
+            if j == 0:
+                entrada[i][4] = entrada[i][4] / L1Max
+                entrada[i][5] = entrada[i][5] / indRefReal_mode1_L1Max
+                entrada[i][6] = entrada[i][6] / indRefImag_mode1_L1Max
+            if j == 1:
+                entrada[i][4] = entrada[i][4] / L2Max
+                entrada[i][5] = entrada[i][5] / indRefReal_mode1_L2Max
+                entrada[i][6] = entrada[i][6] / indRefImag_mode1_L2Max
+            if j == 2:
+                entrada[i][4] = entrada[i][4] / L3Max
+                entrada[i][5] = entrada[i][5] / indRefReal_mode1_L3Max
+                entrada[i][6] = entrada[i][6] / indRefImag_mode1_L3Max
+            if j == 3:
+                entrada[i][4] = entrada[i][4] / L4Max
+                entrada[i][5] = entrada[i][5] / indRefReal_mode1_L4Max
+                entrada[i][6] = entrada[i][6] / indRefImag_mode1_L4Max
+        else:
+            entrada[i][0] = entrada[i][0] / radioComponent2Max
+            entrada[i][1] = entrada[i][1] / sigmaComponent2Max
+            entrada[i][2] = entrada[i][2] / esfericidadComponent2Max
+            entrada[i][3] = entrada[i][3] / concentracionComponent2Max
+            if j == 4:
+                entrada[i][4] = entrada[i][4] / L1Max
+                entrada[i][5] = entrada[i][5] / indRefReal_mode2_L1Max
+                entrada[i][6] = entrada[i][6] / indRefImag_mode2_L1Max
+            if j == 5:
+                entrada[i][4] = entrada[i][4] / L2Max
+                entrada[i][5] = entrada[i][5] / indRefReal_mode2_L2Max
+                entrada[i][6] = entrada[i][6] / indRefImag_mode2_L2Max
+            if j == 6:
+                entrada[i][4] = entrada[i][4] / L3Max
+                entrada[i][5] = entrada[i][5] / indRefReal_mode2_L3Max
+                entrada[i][6] = entrada[i][6] / indRefImag_mode2_L3Max
+            if j == 7:
+                entrada[i][4] = entrada[i][4] / L4Max
+                entrada[i][5] = entrada[i][5] / indRefReal_mode2_L4Max
+                entrada[i][6] = entrada[i][6] / indRefImag_mode2_L4Max
+                j = -1                    
         
         j += 1
     
