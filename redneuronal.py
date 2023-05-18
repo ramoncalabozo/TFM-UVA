3e1454d4dfa47c15642073441f0f0158276ffeca
ramoncalabozo <ramoncalabozo@gmail.com>
2023-05-18 17:22:23 +0200

    resultados por parametro de salida

--- redneuronal.py
+++ redneuronal.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 from tensorflow import keras
 import numpy as np
+import matplotlib.pyplot as plt
 
 def crearModelo(entradaEntrenamiento, salidaEntrenamiento, entradaTest, salidaTest):
 
@@ -11,7 +12,7 @@ def crearModelo(entradaEntrenamiento, salidaEntrenamiento, entradaTest, salidaTe
         # Dropout con tasa de abandono del 20%
         keras.layers.Dropout(0.2),
         # Capa oculta con 8 neuronas y función de activación ReLU
-        keras.layers.Dense(32, activation='relu'),
+        keras.layers.Dense(128, activation='relu'),
         # Capa densa de salida con 3 neuronas y función de activación softmax
         keras.layers.Dense(3)
     ])
@@ -38,6 +39,101 @@ def crearModelo(entradaEntrenamiento, salidaEntrenamiento, entradaTest, salidaTe
     test_loss, test_acc = model.evaluate(entradaTest, salidaTest)
     print("Test_accuracy: " +  str(test_acc) + "\n" + "Test_losser " + str(test_loss))
           
+
+    prediccion = model.predict(entradaTest)
+#EL AOD LO MULTIPLICO POR 2 PORQUE ESE ESTABA NORMALIZADO
+    plt.plot((2*salidaTest[:,0]),2*prediccion[:,0],'.')
+    plt.title('AOD')
+    plt.ylabel('$AOD_{pred}$')
+    plt.xlabel('$AOD_{ref}$')
+    
+    p=np.polyfit(2*salidaTest[:,0],2*prediccion[:,0],1) 
+    r=np.corrcoef(2*salidaTest[:,0],2*prediccion[:,0])
+    r2=r[0,1]*r[1,0]
+    
+    plt.plot([0, 2],[p[0]*0+p[1],p[0]*2+p[1]],'k')
+    plt.text(0, 2, 'y='+str(np.round(p[1]*1000)/1000)+'+'+str(np.round(p[0]*1000)/1000)+'x', fontsize = 12)
+    plt.text(0, 1.8, '$r^2$='+str(np.round(r2*1000)/1000), fontsize = 12)
+    plt.show()
+    
+# EL SSA
+    plt.plot((salidaTest[:,1]),prediccion[:,1],'.')
+    plt.title('SSA')
+    plt.ylabel('$SSA_{pred}$')
+    plt.xlabel('$SSA_{ref}$')
+    
+    p=np.polyfit(salidaTest[:,1],prediccion[:,1],1) 
+    r=np.corrcoef(salidaTest[:,1],prediccion[:,1])
+    r2=r[0,1]*r[1,0]
+    
+    plt.plot([0, 1],[p[0]*0+p[1],p[0]*1+p[1]],'k')
+    plt.text(0, 1, 'y='+str(np.round(p[1]*1000)/1000)+'+'+str(np.round(p[0]*1000)/1000)+'x', fontsize = 12)
+    plt.text(0, 0.9, '$r^2$='+str(np.round(r2*1000)/1000), fontsize = 12)
+    plt.show()
+
+# EL G
+    plt.plot((salidaTest[:,2]),prediccion[:,2],'.')
+    plt.title('g')
+    plt.ylabel('$g_{pred}$')
+    plt.xlabel('$g_{ref}$')
+    
+    p=np.polyfit(salidaTest[:,2],prediccion[:,2],1) 
+    r=np.corrcoef(salidaTest[:,2],prediccion[:,2])
+    r2=r[0,1]*r[1,0]
+    
+    plt.plot([0, 1],[p[0]*0+p[1],p[0]*1+p[1]],'k')
+    plt.text(0, 1, 'y='+str(np.round(p[1]*1000)/1000)+'+'+str(np.round(p[0]*1000)/1000)+'x', fontsize = 12)
+    plt.text(0, 0.9, '$r^2$='+str(np.round(r2*1000)/1000), fontsize = 12)
+    plt.show()
+
+# LOS HISTOGRAMAS
+    diferenciaAOD=2*salidaTest[:,0]-2*prediccion[:,0]
+    diferenciaSSA=salidaTest[:,1]-prediccion[:,1]
+    diferenciaG=salidaTest[:,2]-prediccion[:,2]
+    Nbins=200
+
+# EL AOD    
+    n=plt.hist(diferenciaAOD,bins=Nbins)
+    plt.title('AOD')
+    plt.ylabel('N')
+    plt.xlabel('$AOD_{pred}$-$AOD_{ref}$')
+
+    MBE=np.mean(diferenciaAOD) 
+    Md=np.median(diferenciaAOD) 
+    STD=np.std(diferenciaAOD) 
+    plt.text(np.min(diferenciaAOD), np.max(n[0])*0.95, 'Mean='+str(np.round(MBE*1000)/1000), fontsize = 12)
+    plt.text(np.min(diferenciaAOD), np.max(n[0])*0.88, 'Median='+str(np.round(Md*1000)/1000), fontsize = 12)
+    plt.text(np.min(diferenciaAOD), np.max(n[0])*0.81, 'STD='+str(np.round(STD*1000)/1000), fontsize = 12)
+    plt.show()
+    
+# EL SSA    
+    n=plt.hist(diferenciaSSA,bins=Nbins)
+    plt.title('SSA')
+    plt.ylabel('N')
+    plt.xlabel('$SSA_{pred}$-$SSA_{ref}$')
+
+    MBE=np.mean(diferenciaSSA) 
+    Md=np.median(diferenciaSSA) 
+    STD=np.std(diferenciaSSA) 
+    plt.text(np.min(diferenciaSSA), np.max(n[0])*0.95, 'Mean='+str(np.round(MBE*1000)/1000), fontsize = 12)
+    plt.text(np.min(diferenciaSSA), np.max(n[0])*0.88, 'Median='+str(np.round(Md*1000)/1000), fontsize = 12)
+    plt.text(np.min(diferenciaSSA), np.max(n[0])*0.81, 'STD='+str(np.round(STD*1000)/1000), fontsize = 12)
+    plt.show()
+    
+    
+# EL G    
+    n=plt.hist(diferenciaG,bins=Nbins)
+    plt.title('g')
+    plt.ylabel('N')
+    plt.xlabel('$g_{pred}$-$g_{ref}$')
+
+    MBE=np.mean(diferenciaG) 
+    Md=np.median(diferenciaG) 
+    STD=np.std(diferenciaG) 
+    plt.text(np.min(diferenciaG), np.max(n[0])*0.95, 'Mean='+str(np.round(MBE*1000)/1000), fontsize = 12)
+    plt.text(np.min(diferenciaG), np.max(n[0])*0.88, 'Median='+str(np.round(Md*1000)/1000), fontsize = 12)
+    plt.text(np.min(diferenciaG), np.max(n[0])*0.81, 'STD='+str(np.round(STD*1000)/1000), fontsize = 12)
+    plt.show()
 
 def recolectarDatos(archivoDataSet, numEntrenamiento):
     entradaEntrenamiento = []
