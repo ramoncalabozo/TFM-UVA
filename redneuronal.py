5954b16b0630cb1e604af3f9449885b031a75be2
ramoncalabozo <ramoncalabozo@gmail.com>
2023-09-01 00:42:11 +0200

    Añadido graficos, y dividido todo en funciones

--- redneuronal.py
+++ redneuronal.py
@@ -3,6 +3,244 @@ from tensorflow import keras
 import numpy as np
 import matplotlib.pyplot as plt
 
+def pintarPredicciones(prediccion, salidaTest):
+
+# EL AOD
+    plt.plot((salidaTest[:,0]),prediccion[:,0],'.')
+    plt.title('AOD')
+    plt.ylabel('$AOD_{pred}$')
+    plt.xlabel('$AOD_{ref}$')
+    
+    p=np.polyfit(salidaTest[:,0],prediccion[:,0],1) 
+    r=np.corrcoef(salidaTest[:,0],prediccion[:,0])
+    r2=r[0,1]*r[1,0]
+    
+    plt.plot([0, 2],[p[0]*0+p[1],p[0]*2+p[1]],'k')
+    plt.text(0, 2, 'y='+str(np.round(p[1]*1000)/1000)+'+'+str(np.round(p[0]*1000)/1000)+'x', fontsize = 12)
+    plt.text(0, 1.8, '$r^2$='+str(np.round(r2*1000)/1000), fontsize = 12)
+    plt.show()
+    
+# EL SSA
+    plt.plot((salidaTest[:,1]),prediccion[:,1],'.')
+    plt.title('SSA')
+    plt.ylabel('$SSA_{pred}$')
+    plt.xlabel('$SSA_{ref}$')
+    
+    p=np.polyfit(salidaTest[:,1],prediccion[:,1],1) 
+    r=np.corrcoef(salidaTest[:,1],prediccion[:,1])
+    r2=r[0,1]*r[1,0]
+    
+    plt.plot([0, 1],[p[0]*0+p[1],p[0]*1+p[1]],'k')
+    plt.text(0, 1, 'y='+str(np.round(p[1]*1000)/1000)+'+'+str(np.round(p[0]*1000)/1000)+'x', fontsize = 12)
+    plt.text(0, 0.9, '$r^2$='+str(np.round(r2*1000)/1000), fontsize = 12)
+    plt.show()
+
+# EL G
+    plt.plot((salidaTest[:,2]),prediccion[:,2],'.')
+    plt.title('g')
+    plt.ylabel('$g_{pred}$')
+    plt.xlabel('$g_{ref}$')
+    
+    p=np.polyfit(salidaTest[:,2],prediccion[:,2],1) 
+    r=np.corrcoef(salidaTest[:,2],prediccion[:,2])
+    r2=r[0,1]*r[1,0]
+    
+    plt.plot([0, 1],[p[0]*0+p[1],p[0]*1+p[1]],'k')
+    plt.text(0, 1, 'y='+str(np.round(p[1]*1000)/1000)+'+'+str(np.round(p[0]*1000)/1000)+'x', fontsize = 12)
+    plt.text(0, 0.9, '$r^2$='+str(np.round(r2*1000)/1000), fontsize = 12)
+    plt.show()
+
+def pintarResultadosModelo(history):
+    # summarize history for accuracy
+    plt.plot(history.history['accuracy'])
+    plt.plot(history.history['val_accuracy'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
+    # summarize history for loss
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
+
+def pintarDiferencias(diferenciaAOD, diferenciaSSA, diferenciaG):
+    # AOD
+
+    plt.plot((entradaTest[:,0])*escala[0],diferenciaAOD,'.')
+    #plt.title('Radio')
+    plt.xlabel('Radio')
+    plt.ylabel('$AOD_{pred}$-$AOD_{ref}$')
+    plt.show()
+
+    plt.plot((entradaTest[:,1])*escala[1],diferenciaAOD,'.')
+    # plt.title('Concentracion')
+    plt.xlabel('Sigma')
+    plt.ylabel('$AOD_{pred}$-$AOD_{ref}$')
+    plt.show()
+
+    plt.plot((entradaTest[:,2])*escala[2],diferenciaAOD,'.')
+    # plt.title('difAOD')
+    plt.xlabel('esfericiad')
+    plt.ylabel('$AOD_{pred}$-$AOD_{ref}$')
+    plt.show()
+
+    plt.plot((entradaTest[:,3])*escala[3],diferenciaAOD,'.')
+    plt.xlabel('Concentracion')
+    plt.ylabel('$AOD_{pred}$-$AOD_{ref}$')
+    plt.show()
+
+    plt.plot((entradaTest[:,4])*escala[4],diferenciaAOD,'.')
+    #plt.title('difAOD')
+    plt.xlabel('longitud de onda')
+    plt.ylabel('$AOD_{pred}$-$AOD_{ref}$')
+    plt.show()
+
+    plt.plot((entradaTest[:,5])*escala[5],diferenciaAOD,'.')
+    plt.xlabel('RRI')
+    plt.ylabel('$AOD_{pred}$-$AOD_{ref}$')
+    plt.show()
+
+    plt.plot((entradaTest[:,6])*escala[6],diferenciaAOD,'.')
+    plt.xlabel('IMI')
+    plt.ylabel('$AOD_{pred}$-$AOD_{ref}$')
+    plt.show()
+    
+
+    
+    #SSA
+
+    plt.plot((entradaTest[:,0])*escala[0],diferenciaSSA,'.')
+    #plt.title('Radio')
+    plt.xlabel('Radio')
+    plt.ylabel('$SSA{pred}$-$SSA{ref}$')
+    plt.show()
+
+    plt.plot((entradaTest[:,1])*escala[1],diferenciaSSA,'.')
+    # plt.title('Concentracion')
+    plt.xlabel('Sigma')
+    plt.ylabel('$SSA{pred}$-$SSA{ref}$')
+    plt.show()
+
+    plt.plot((entradaTest[:,2])*escala[2],diferenciaSSA,'.')
+    # plt.title('difAOD')
+    plt.xlabel('esfericiad')
+    plt.ylabel('$SSA{pred}$-$SSA{ref}$')
+    plt.show()
+
+    plt.plot((entradaTest[:,3])*escala[3],diferenciaSSA,'.')
+    plt.xlabel('Concentracion')
+    plt.ylabel('$SSA{pred}$-$SSA{ref}$')
+    plt.show()
+
+    plt.plot((entradaTest[:,4])*escala[4],diferenciaSSA,'.')
+    #plt.title('difAOD')
+    plt.xlabel('longitud de onda')
+    plt.ylabel('$SSA{pred}$-$SSA{ref}$')
+    plt.show()
+
+    plt.plot((entradaTest[:,5])*escala[5],diferenciaSSA,'.')
+    plt.xlabel('RRI')
+    plt.ylabel('$SSA{pred}$-$SSA{ref}$')
+    plt.show()
+
+    plt.plot((entradaTest[:,6])*escala[6],diferenciaSSA,'.')
+    plt.xlabel('IMI')
+    plt.ylabel('$SSA{pred}$-$SSA{ref}$')
+    plt.show()
+
+
+
+
+    #G
+
+    plt.plot((entradaTest[:,0])*escala[0],diferenciaG,'.')
+    #plt.title('Radio')
+    plt.xlabel('Radio')
+    plt.ylabel('$G{pred}$-$G{ref}$')
+    plt.show()
+
+    plt.plot((entradaTest[:,1])*escala[1],diferenciaG,'.')
+    # plt.title('Concentracion')
+    plt.xlabel('Sigma')
+    plt.ylabel('$G{pred}$-$G{ref}$')
+    plt.show()
+
+    plt.plot((entradaTest[:,2])*escala[2],diferenciaG,'.')
+    # plt.title('difAOD')
+    plt.xlabel('esfericiad')
+    plt.ylabel('$G{pred}$-$G{ref}$')
+    plt.show()
+
+    plt.plot((entradaTest[:,3])*escala[3],diferenciaG,'.')
+    plt.xlabel('Concentracion')
+    plt.ylabel('$G{pred}$-$G{ref}$')
+    plt.show()
+
+    plt.plot((entradaTest[:,4])*escala[4],diferenciaG,'.')
+    #plt.title('difAOD')
+    plt.xlabel('longitud de onda')
+    plt.ylabel('$G{pred}$-$G{ref}$')
+    plt.show()
+
+    plt.plot((entradaTest[:,5])*escala[5],diferenciaG,'.')
+    plt.xlabel('RRI')
+    plt.ylabel('$G{pred}$-$G{ref}$')
+    plt.show()
+
+    plt.plot((entradaTest[:,6])*escala[6],diferenciaG,'.')
+    plt.xlabel('IMI')
+    plt.ylabel('$G{pred}$-$G{ref}$')
+    plt.show()
+
+def pintarHistogramas(diferenciaAOD, diferenciaSSA, diferenciaG, Nbins):
+# EL AOD    
+    n=plt.hist(diferenciaAOD,bins=Nbins)
+    plt.title('AOD')
+    plt.ylabel('N')
+    plt.xlabel('$AOD_{pred}$-$AOD_{ref}$')
+
+    MBE=np.mean(diferenciaAOD) 
+    Md=np.median(diferenciaAOD) 
+    STD=np.std(diferenciaAOD) 
+    plt.text(np.min(diferenciaAOD), np.max(n[0])*0.95, 'Mean='+str(np.round(MBE*1000)/1000), fontsize = 12)
+    plt.text(np.min(diferenciaAOD), np.max(n[0])*0.88, 'Median='+str(np.round(Md*1000)/1000), fontsize = 12)
+    plt.text(np.min(diferenciaAOD), np.max(n[0])*0.81, 'STD='+str(np.round(STD*1000)/1000), fontsize = 12)
+    plt.show()
+    
+# EL SSA    
+    n=plt.hist(diferenciaSSA,bins=Nbins)
+    plt.title('SSA')
+    plt.ylabel('N')
+    plt.xlabel('$SSA_{pred}$-$SSA_{ref}$')
+
+    MBE=np.mean(diferenciaSSA) 
+    Md=np.median(diferenciaSSA) 
+    STD=np.std(diferenciaSSA) 
+    plt.text(np.min(diferenciaSSA), np.max(n[0])*0.95, 'Mean='+str(np.round(MBE*1000)/1000), fontsize = 12)
+    plt.text(np.min(diferenciaSSA), np.max(n[0])*0.88, 'Median='+str(np.round(Md*1000)/1000), fontsize = 12)
+    plt.text(np.min(diferenciaSSA), np.max(n[0])*0.81, 'STD='+str(np.round(STD*1000)/1000), fontsize = 12)
+    plt.show()
+    
+    
+# EL G    
+    n=plt.hist(diferenciaG,bins=Nbins)
+    plt.title('g')
+    plt.ylabel('N')
+    plt.xlabel('$g_{pred}$-$g_{ref}$')
+
+    MBE=np.mean(diferenciaG) 
+    Md=np.median(diferenciaG) 
+    STD=np.std(diferenciaG) 
+    plt.text(np.min(diferenciaG), np.max(n[0])*0.95, 'Mean='+str(np.round(MBE*1000)/1000), fontsize = 12)
+    plt.text(np.min(diferenciaG), np.max(n[0])*0.88, 'Median='+str(np.round(Md*1000)/1000), fontsize = 12)
+    plt.text(np.min(diferenciaG), np.max(n[0])*0.81, 'STD='+str(np.round(STD*1000)/1000), fontsize = 12)
+    plt.show()
+
 def crearModelo(entradaEntrenamiento, salidaEntrenamiento, entradaTest, salidaTest, escala):
     # Crear el modelo de red neuronal 
     model = keras.Sequential([
@@ -48,23 +286,8 @@ def crearModelo(entradaEntrenamiento, salidaEntrenamiento, entradaTest, salidaTe
 
     # VEO LOS RESULTADOS DEL MODELO
     print(history.history.keys())
-    # summarize history for accuracy
-    plt.plot(history.history['accuracy'])
-    plt.plot(history.history['val_accuracy'])
-    plt.title('model accuracy')
-    plt.ylabel('accuracy')
-    plt.xlabel('epoch')
-    plt.legend(['train', 'test'], loc='upper left')
-    plt.show()
-    # summarize history for loss
-    plt.plot(history.history['loss'])
-    plt.plot(history.history['val_loss'])
-    plt.title('model loss')
-    plt.ylabel('loss')
-    plt.xlabel('epoch')
-    plt.legend(['train', 'test'], loc='upper left')
-    plt.show()
-
+    
+    pintarResultadosModelo(history)
     
     print("\n\n\nEVALUACIÓN: \n")
     model=tf.keras.models.load_model( 'MejorVersionModelo.h5')
@@ -75,49 +298,8 @@ def crearModelo(entradaEntrenamiento, salidaEntrenamiento, entradaTest, salidaTe
 
 
     prediccion = model.predict(entradaTest)
-    plt.plot((salidaTest[:,0]),prediccion[:,0],'.')
-    plt.title('AOD')
-    plt.ylabel('$AOD_{pred}$')
-    plt.xlabel('$AOD_{ref}$')
-    
-    p=np.polyfit(salidaTest[:,0],prediccion[:,0],1) 
-    r=np.corrcoef(salidaTest[:,0],prediccion[:,0])
-    r2=r[0,1]*r[1,0]
-    
-    plt.plot([0, 2],[p[0]*0+p[1],p[0]*2+p[1]],'k')
-    plt.text(0, 2, 'y='+str(np.round(p[1]*1000)/1000)+'+'+str(np.round(p[0]*1000)/1000)+'x', fontsize = 12)
-    plt.text(0, 1.8, '$r^2$='+str(np.round(r2*1000)/1000), fontsize = 12)
-    plt.show()
-    
-# EL SSA
-    plt.plot((salidaTest[:,1]),prediccion[:,1],'.')
-    plt.title('SSA')
-    plt.ylabel('$SSA_{pred}$')
-    plt.xlabel('$SSA_{ref}$')
-    
-    p=np.polyfit(salidaTest[:,1],prediccion[:,1],1) 
-    r=np.corrcoef(salidaTest[:,1],prediccion[:,1])
-    r2=r[0,1]*r[1,0]
-    
-    plt.plot([0, 1],[p[0]*0+p[1],p[0]*1+p[1]],'k')
-    plt.text(0, 1, 'y='+str(np.round(p[1]*1000)/1000)+'+'+str(np.round(p[0]*1000)/1000)+'x', fontsize = 12)
-    plt.text(0, 0.9, '$r^2$='+str(np.round(r2*1000)/1000), fontsize = 12)
-    plt.show()
 
-# EL G
-    plt.plot((salidaTest[:,2]),prediccion[:,2],'.')
-    plt.title('g')
-    plt.ylabel('$g_{pred}$')
-    plt.xlabel('$g_{ref}$')
-    
-    p=np.polyfit(salidaTest[:,2],prediccion[:,2],1) 
-    r=np.corrcoef(salidaTest[:,2],prediccion[:,2])
-    r2=r[0,1]*r[1,0]
-    
-    plt.plot([0, 1],[p[0]*0+p[1],p[0]*1+p[1]],'k')
-    plt.text(0, 1, 'y='+str(np.round(p[1]*1000)/1000)+'+'+str(np.round(p[0]*1000)/1000)+'x', fontsize = 12)
-    plt.text(0, 0.9, '$r^2$='+str(np.round(r2*1000)/1000), fontsize = 12)
-    plt.show()
+    pintarPredicciones(prediccion, salidaTest)
 
 # LAS DIFERNECIAS
     diferenciaAOD=salidaTest[:,0]-prediccion[:,0]
@@ -125,90 +307,9 @@ def crearModelo(entradaEntrenamiento, salidaEntrenamiento, entradaTest, salidaTe
     diferenciaG=salidaTest[:,2]-prediccion[:,2]
     Nbins=200
 
+    pintarDiferencias(diferenciaAOD, diferenciaSSA, diferenciaG)
 
-    plt.plot((entradaTest[:,0])*escala[0],diferenciaAOD,'.')
-    plt.title('Radio')
-    plt.xlabel('Radio')
-    plt.ylabel('$AOD_{pred}$-$AOD_{ref}$')
-    plt.show()
-
-    plt.plot((entradaTest[:,1])*escala[1],diferenciaAOD,'.')
-    # plt.title('Concentracion')
-    plt.xlabel('Sigma')
-    plt.ylabel('$AOD_{pred}$-$AOD_{ref}$')
-    plt.show()
-
-    plt.plot((entradaTest[:,2])*escala[2],diferenciaAOD,'.')
-    # plt.title('difAOD')
-    plt.xlabel('esfericiad')
-    plt.ylabel('$AOD_{pred}$-$AOD_{ref}$')
-    plt.show()
-
-    plt.plot((entradaTest[:,3])*escala[3],diferenciaAOD,'.')
-    plt.xlabel('Concentracion')
-    plt.ylabel('$AOD_{pred}$-$AOD_{ref}$')
-    plt.show()
-
-    plt.plot((entradaTest[:,4])*escala[4],diferenciaAOD,'.')
-    plt.title('difAOD')
-    plt.xlabel('longitud de onda')
-    plt.ylabel('$AOD_{pred}$-$AOD_{ref}$')
-    plt.show()
-
-    plt.plot((entradaTest[:,5])*escala[5],diferenciaAOD,'.')
-    plt.xlabel('RRI')
-    plt.ylabel('$AOD_{pred}$-$AOD_{ref}$')
-    plt.show()
-
-    plt.plot((entradaTest[:,6])*escala[6],diferenciaAOD,'.')
-    plt.xlabel('IMI')
-    plt.ylabel('$AOD_{pred}$-$AOD_{ref}$')
-    plt.show()
-    
-# LOS HISTOGRAMAS
-# EL AOD    
-    n=plt.hist(diferenciaAOD,bins=Nbins)
-    plt.title('AOD')
-    plt.ylabel('N')
-    plt.xlabel('$AOD_{pred}$-$AOD_{ref}$')
-
-    MBE=np.mean(diferenciaAOD) 
-    Md=np.median(diferenciaAOD) 
-    STD=np.std(diferenciaAOD) 
-    plt.text(np.min(diferenciaAOD), np.max(n[0])*0.95, 'Mean='+str(np.round(MBE*1000)/1000), fontsize = 12)
-    plt.text(np.min(diferenciaAOD), np.max(n[0])*0.88, 'Median='+str(np.round(Md*1000)/1000), fontsize = 12)
-    plt.text(np.min(diferenciaAOD), np.max(n[0])*0.81, 'STD='+str(np.round(STD*1000)/1000), fontsize = 12)
-    plt.show()
-    
-# EL SSA    
-    n=plt.hist(diferenciaSSA,bins=Nbins)
-    plt.title('SSA')
-    plt.ylabel('N')
-    plt.xlabel('$SSA_{pred}$-$SSA_{ref}$')
-
-    MBE=np.mean(diferenciaSSA) 
-    Md=np.median(diferenciaSSA) 
-    STD=np.std(diferenciaSSA) 
-    plt.text(np.min(diferenciaSSA), np.max(n[0])*0.95, 'Mean='+str(np.round(MBE*1000)/1000), fontsize = 12)
-    plt.text(np.min(diferenciaSSA), np.max(n[0])*0.88, 'Median='+str(np.round(Md*1000)/1000), fontsize = 12)
-    plt.text(np.min(diferenciaSSA), np.max(n[0])*0.81, 'STD='+str(np.round(STD*1000)/1000), fontsize = 12)
-    plt.show()
-    
-    
-# EL G    
-    n=plt.hist(diferenciaG,bins=Nbins)
-    plt.title('g')
-    plt.ylabel('N')
-    plt.xlabel('$g_{pred}$-$g_{ref}$')
-
-    MBE=np.mean(diferenciaG) 
-    Md=np.median(diferenciaG) 
-    STD=np.std(diferenciaG) 
-    plt.text(np.min(diferenciaG), np.max(n[0])*0.95, 'Mean='+str(np.round(MBE*1000)/1000), fontsize = 12)
-    plt.text(np.min(diferenciaG), np.max(n[0])*0.88, 'Median='+str(np.round(Md*1000)/1000), fontsize = 12)
-    plt.text(np.min(diferenciaG), np.max(n[0])*0.81, 'STD='+str(np.round(STD*1000)/1000), fontsize = 12)
-    plt.show()
-
+    pintarHistogramas(diferenciaAOD, diferenciaSSA, diferenciaG, Nbins)
     
 def recolectarDatos(archivoDataSet, numEntrenamiento):
     entradaEntrenamiento = []
@@ -245,7 +346,6 @@ def recolectarDatos(archivoDataSet, numEntrenamiento):
     entradaTest = np.array(entradaTest)
     salidaTest = np.array(salidaTest)
     return entradaEntrenamiento, salidaEntrenamiento, entradaTest, salidaTest
-
 
 def normalizarDatosEntradas(parametros):
     cicle = 0
